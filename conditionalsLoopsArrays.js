86295c568b18bc86d1c4ec0ebefce0fb0fddc0dc
Jayden-ASC5 <smith.jayden1010@gmail.com>
2019-07-11 11:12:01 -0400

    challenge

--- conditionalsLoopsArrays.js
+++ conditionalsLoopsArrays.js
@@ -6,36 +6,53 @@ console.log("Challenge #1:");
 // Create an array called "roster" with the following names as 
 // strings: Pikachu, MewTwo, Onix, Eevee
 // Console.log the length of the "roster" array
-
+ const roster = ['Pikachu', 'MewTwo', 'Onix', 'Eevee'];
+ console.log(roster.length);
 // ==================================================================
 console.log("Challenge #2:");
 // push the user's first command line argument to the end of the 
 // "roster" array (ideally, it's a pokemon's name!)
 // console.log the "roster" array
-
+roster.push(process.argv[2]);
+console.log(roster)
 // ==================================================================
 console.log("Challenge #3:");
 // remove the item from the "roster" array that corresponds to 
 // the user's second command line argument
 // NOTE: consider what data type command line arguments come in as
 // console.log the "roster" array
+if(process.argv[3] == "Pikachu"){
+    roster.splice(0, 1)
+}
+ if(process.argv[3] == "MewTwo"){
+     roster.splice(1, 1)
+ }
+if(process.argv[3] == "Onix"){
+    roster.splice(2, 1)
+ }
+ if(process.argv[3] == "Eevee"){
+    roster.splice(3, 1)
+};
 
+console.log(roster);
 //////////////////
 // CONDITIONALS //
 //////////////////
 console.log("Challenge #4:");
 // Create a variable called "catchChance" and set it equal to .25
 // Note: this corresponds to a 25% chance to catch a pokemon!
-
+let catchChance = .25;
 // Create a variable called "catchAttempt" and set it equal to 
 // Math.random()
-
+let catchAttempt = Math.random();
 // Note: we'll learn more about Math.random() later, but for now
 // just think of it as equal to a random decimal between 0 and 1
 
 // If "catchAttempt" is less than "catchChance" console.log "Caught it!"
 // Otherwise, console.log "Aargh! Almost had it!"
-
+if(catchAttempt < catchChance){
+    console.log("Caught it!")
+}
 // ==================================================================
 console.log("Challenge #5:");
 // If the number of pokemon in the "roster" array is less than 6
